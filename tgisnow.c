001c601abf2642cf9459e5ee60f43f4c01e1b6fa
Mik <44463908+javalonde@users.noreply.github.com>
2024-08-26 19:30:02 +0200

    no sprite, but joystick added

--- tgisnow.c
+++ tgisnow.c
@@ -8,43 +8,23 @@
 
 #define BITMAP_WIDTH 35
 #define BITMAP_HEIGHT 32
-#define SCREEN_WIDTH 160 // Szerokość ekranu w trybie Antic Mode F (160 pikseli)
+#define SCREEN_WIDTH 160
 #define SCREEN_HEIGHT 80
 #define BYTES_PER_LINE (BITMAP_WIDTH / 4)
 #define SCREEN_BYTES_PER_LINE (SCREEN_WIDTH / 4)
 #define BITMAP_SIZE (BITMAP_WIDTH * BITMAP_HEIGHT / 4)
 #define BUFFER_SIZE (SCREEN_BYTES_PER_LINE * BITMAP_HEIGHT)
 
-#define PMG_BASE 0xC000
-#define SPRITE_WIDTH 8
-#define SPRITE_HEIGHT 8
-#define SPRITE_POS_X 0x64   // Pozycja X sprite'a w heksadecymalnej
-#define SPRITE_POS_Y 0x32   // Pozycja Y sprite'a w heksadecymalnej
-
 // Makra do zapisu do pamięci
 #define poke(addr, val) (*(unsigned char*) (addr) = (val))
 #define pokew(addr, val) (*(unsigned int*) (addr) = (val))
 
-// Definicja danych duszka - napis ATARI (8x8 pikseli)
-const unsigned char atari_sprite[] = {
-    0xFF, // AAAAAAAA
-    0x81, // A......A
-    0xBD, // A.XXXX.X
-    0xBD, // A.XXXX.X
-    0xBD, // A.XXXX.X
-    0xBD, // A.XXXX.X
-    0x81, // A......A
-    0xFF  // AAAAAAAA
-};
-
-// Asemblerowa funkcja kopiująca blok pamięci (używana do rysowania)
-void mover(int n1, const unsigned char *n2, int n3, int n4, int n5)
-{
-    pokew(0xCB, n1);                 // Adres docelowy w pamięci ekranu
-    pokew(0xCD, (unsigned int)n2);   // Rzutowanie wskaźnika na typ całkowity
-    poke(0xCF, n3);                  // Liczba bajtów na wiersz
-    poke(0xD2, n4);                  // Liczba wierszy
-    poke(0xD0, n5);                  // Liczba bajtów na wiersz w pamięci ekranu
+void mover(int n1, const unsigned char *n2, int n3, int n4, int n5) {
+    pokew(0xCB, n1);
+    pokew(0xCD, (unsigned int)n2);
+    poke(0xCF, n3);
+    poke(0xD2, n4);
+    poke(0xD0, n5);
 
     asm("  LDX $D2");
     asm("_label4:");
@@ -74,158 +54,139 @@ void mover(int n1, const unsigned char *n2, int n3, int n4, int n5)
     asm("  BNE _label4");
 }
 
-// Funkcja aktywująca display list
 void activate_display_list(char* dl) {
-    POKE(0x230, (unsigned char)((unsigned int)dl & 0xFF));
-    POKE(0x231, (unsigned char)(((unsigned int)dl >> 8) & 0xFF));
+    POKE(0x230, (unsigned int)dl & 0xFF);
+    POKE(0x231, ((unsigned int)dl >> 8) & 0xFF);
 }
 
-// Funkcja czekająca na VBlank
 void wait_for_vblank() {
-    while (PEEK(0xD40B) != 0) {}  // Wait for VCOUNT to reset to 0
+    while (PEEK(0xD40B) != 0) {}
 }
 
-// Funkcja tworząca display list dla bitmapy 35x32
 char* create_display_list(unsigned char* bitmap_data) {
     static unsigned char dl[1024];
     unsigned char* p = dl;
     unsigned int addr = (unsigned int)bitmap_data;
     int i;
 
-    // Display List Setup for Antic Mode F (4-color mode)
-    *p++ = 0x70; // 8 pustych linii
+    *p++ = 0x70;
     *p++ = 0x70;
     *p++ = 0x70;
 
-    // Load Memory Scan (LMS) with Antic Mode F
-    *p++ = 0x4F; // LMS + Antic Mode F
-    *p++ = (unsigned char)(addr & 0xFF);
-    *p++ = (unsigned char)((addr >> 8) & 0xFF);
+    *p++ = 0x4F;
+    *p++ = addr & 0xFF;
+    *p++ = (addr >> 8) & 0xFF;
 
-    // Pozostałe linie ekranu w Antic Mode F
     for (i = 0; i < BITMAP_HEIGHT; ++i) {
-        *p++ = 0x4F; // Antic Mode F
-        *p++ = (unsigned char)((addr + i * SCREEN_BYTES_PER_LINE) & 0xFF);
-        *p++ = (unsigned char)(((addr + i * SCREEN_BYTES_PER_LINE) >> 8) & 0xFF);
+        *p++ = 0x4F;
+        *p++ = (addr + i * SCREEN_BYTES_PER_LINE) & 0xFF;
+        *p++ = ((addr + i * SCREEN_BYTES_PER_LINE) >> 8) & 0xFF;
     }
 
-    // Wypełnienie reszty ekranu pustymi liniami
     for (; i < SCREEN_HEIGHT; ++i) {
-        *p++ = 0x70; // Pusta linia
+        *p++ = 0x70;
     }
 
-    // Koniec Display List
-    *p++ = 0x41;  // JVB
-    *p++ = (unsigned char)((unsigned int)dl & 0xFF);
-    *p++ = (unsigned char)(((unsigned int)dl >> 8) & 0xFF);
+    *p++ = 0x41;
+    *p++ = (unsigned int)dl & 0xFF;
+    *p++ = ((unsigned int)dl >> 8) & 0xFF;
 
     return (char*)dl;
 }
 
-// Funkcja rysująca tło z przesunięciem w poziomie
 void draw_background(unsigned char* buffer_memory, int offset) {
     int y;
     for (y = 0; y < BITMAP_HEIGHT; ++y) {
-        const unsigned char* tile_ptr = background_tile + ((64 - offset + y) % 64) * 32; // 64 rows, each row 32 bytes
+        const unsigned char* tile_ptr = background_tile + ((64 + offset + y) % 64) * 32;
         mover((int)(buffer_memory + y * SCREEN_BYTES_PER_LINE), tile_ptr, SCREEN_BYTES_PER_LINE, 1, SCREEN_BYTES_PER_LINE);
     }
 }
 
-// Funkcja rysująca żabkę na ekranie z uwzględnieniem zerowych danych
-void draw_frog(unsigned char* buffer_memory, const unsigned char* frame_data) {
+void draw_frog(unsigned char* buffer_memory, const unsigned char* frame_data, int frog_position) {
     int y, x;
     for (y = 0; y < BITMAP_HEIGHT; ++y) {
         for (x = 0; x < BYTES_PER_LINE; ++x) {
             unsigned char pixel = frame_data[y * BYTES_PER_LINE + x];
             if (pixel != 0x00) {
-                mover((int)(buffer_memory + y * SCREEN_BYTES_PER_LINE + (SCREEN_BYTES_PER_LINE / 2 - BYTES_PER_LINE / 2) + x), 
-                      &pixel, 1, 1, SCREEN_BYTES_PER_LINE);
+                mover((int)(buffer_memory + y * SCREEN_BYTES_PER_LINE + frog_position + x), &pixel, 1, 1, SCREEN_BYTES_PER_LINE);
             }
         }
     }
 }
 
-// Funkcja kopiująca bufor do pamięci ekranu
 void copy_buffer_to_screen(unsigned char* buffer_memory, unsigned char* screen_memory) {
     memcpy(screen_memory, buffer_memory, BUFFER_SIZE);
 }
 
-// Funkcja inicjalizująca PMG (Player/Missile Graphics)
-void init_pmg() {
-    // Włączenie PMG
-    POKE(0xD00D, 0x03);  // GRACTL - Włączenie PMG dla graczy i pocisków
-    POKE(0xD400, 0x22);  // DMACTL - Włączenie DMA dla PMG
-
-    // Ustawienie bazowej pamięci PMG na 0xC000
-    POKE(0xD407, 0xC0); // PMBASE ustawiony na 0xC000
-    memset((void*)PMG_BASE, 0, 512); // Wyczyszczenie pamięci PMG
-
-    // Skopiowanie danych sprite'a do pamięci PMG
-    memcpy((void*)(PMG_BASE + 0x380), atari_sprite, SPRITE_HEIGHT);
-
-    // Ustawienie koloru gracza 0 na różowy
-    POKE(0xD012, 0xE0); // Kolor duszka (różowy)
-
-    // Ustawienie początkowej pozycji sprite'a
-    POKE(0xD000, SPRITE_POS_X); // Pozycja X gracza 0
-    POKE(0xD001, SPRITE_POS_Y); // Pozycja Y gracza 0
+void joystick_control(int *frog_position) {
+    int s = PEEK(0x278);  // Odczyt stanu joysticka
+    if (s < 12) {
+        *frog_position += (8 - s > 0) ? 2 : -2;  // Lewo/Prawo
+        if (*frog_position < 0) *frog_position = 0;
+        if (*frog_position > SCREEN_BYTES_PER_LINE - BYTES_PER_LINE) *frog_position = SCREEN_BYTES_PER_LINE - BYTES_PER_LINE;
+    }
 }
 
-// Główna funkcja
 void main(void) {
     char* dl;
     unsigned char* screen_memory = (unsigned char*)0x4000;
-    unsigned char buffer_memory[BUFFER_SIZE]; // Bufor do podwójnego buforowania
+    unsigned char buffer_memory[BUFFER_SIZE];
     int offset = 0;
-    int sprite_x = SPRITE_POS_X, sprite_y = SPRITE_POS_Y;
-    int sprite_dx = 1, sprite_dy = 1;
+    int frog_position = SCREEN_BYTES_PER_LINE / 4 - BYTES_PER_LINE / 4;
 
-    // Inicjalizacja trybu graficznego
     _graphics(10);
     clrscr();
 
-    // Ustawienia palety kolorów
-    POKE(704, 15);  // Kolor tła (biały)
-    POKE(708, 12);  // COLOR0
-    POKE(706, 23);  // COLOR2
-    POKE(707, 7);   // COLOR3
-    POKE(711, 0);   // COLOR4
-    POKE(712, 50);  // COLOR5
+    POKE(704, 15);
+    POKE(708, 12);
+    POKE(706, 23);
+    POKE(707, 7);
+    POKE(711, 0);
+    POKE(712, 50);
 
-    // Tworzenie i aktywacja display list
     dl = create_display_list(screen_memory);
     activate_display_list(dl);
 
-    // Inicjalizacja PMG (sprite'a)
-    init_pmg();
-
-    // Animacja żaby z przesuwającym się tłem i sprite'em
     while (!kbhit()) {
-        wait_for_vblank();  // Synchronize with the VBlank
-        draw_background(buffer_memory, offset);  // Rysowanie przesuniętego tła w buforze
-        draw_frog(buffer_memory, frog1);         // Rysowanie żaby (1 klatka) w buforze
-        copy_buffer_to_screen(buffer_memory, screen_memory);  // Kopiowanie bufora na ekran
-        offset = (offset + 3) % 64;  // Zwiększanie offsetu, aby tło przesuwało się
+        wait_for_vblank();
 
-        // Aktualizacja pozycji sprite'a
-        sprite_x += sprite_dx;
-        sprite_y += sprite_dy;
+        joystick_control(&frog_position);
 
-        // Zmiana kierunku, gdy napotka krawędzie ekranu
-        if (sprite_x <= 0 || sprite_x >= 152) sprite_dx = -sprite_dx;
-        if (sprite_y <= 0 || sprite_y >= 192) sprite_dy = -sprite_dy;
-
-        // Ustawianie pozycji sprite'a (napis "ATARI")
-        POKE(0xD000, sprite_x); // Pozycja X gracza 0
-        POKE(0xD001, sprite_y); // Pozycja Y gracza 0
+        draw_background(buffer_memory, offset);
+        draw_frog(buffer_memory, frog1, frog_position);
+        copy_buffer_to_screen(buffer_memory, screen_memory);
+        offset = (offset + 1) % 64;
 
         wait_for_vblank();
         draw_background(buffer_memory, offset);
-        draw_frog(buffer_memory, frog2);
+        draw_frog(buffer_memory, frog2, frog_position);
         copy_buffer_to_screen(buffer_memory, screen_memory);
-        offset = (offset + 3) % 64;
+        offset = (offset + 2) % 64;
+
+        wait_for_vblank();
+        draw_background(buffer_memory, offset);
+        draw_frog(buffer_memory, frog3, frog_position);
+        copy_buffer_to_screen(buffer_memory, screen_memory);
+        offset = (offset + 2) % 64;
+
+        wait_for_vblank();
+        draw_background(buffer_memory, offset);
+        draw_frog(buffer_memory, frog4, frog_position);
+        copy_buffer_to_screen(buffer_memory, screen_memory);
+        offset = (offset + 2) % 64;
+
+        wait_for_vblank();
+        draw_background(buffer_memory, offset);
+        draw_frog(buffer_memory, frog5, frog_position);
+        copy_buffer_to_screen(buffer_memory, screen_memory);
+        offset = (offset + 2) % 64;
+
+        wait_for_vblank();
+        draw_background(buffer_memory, offset);
+        draw_frog(buffer_memory, frog6, frog_position);
+        copy_buffer_to_screen(buffer_memory, screen_memory);
+        offset = (offset + 2) % 64;
     }
 
-    // Czekanie na naciśnięcie klawisza przed zakończeniem
     cgetc();
 }
